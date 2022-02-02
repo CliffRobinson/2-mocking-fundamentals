5ecb34844b80c3c8e125570dfb36906307110e89
cliffrobinson <cliff@cogo.co>
2022-02-02 16:01:51 +1300

    complete vid 2

--- src/no-framework/monkey-patching.js
+++ src/no-framework/monkey-patching.js
@@ -2,4 +2,12 @@ const assert = require('assert')
 const thumbWar = require('../thumb-war')
 const utils = require('../utils')
 
+const originalGetWinner = utils.getWinner
 
+utils.getWinner = (p1, p2) => p1
+
+const winner = thumbWar('Cliff', 'Not Cliff')
+
+assert.strictEqual(winner, 'Cliff')
+
+utils.getWinner = originalGetWinner
