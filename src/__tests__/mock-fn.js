7a0dbbc540119a459e055112499b3981e4237b1b
cliffrobinson <cliff@cogo.co>
2022-02-02 16:13:41 +1300

    vid 3 part 1

--- src/__tests__/mock-fn.js
+++ src/__tests__/mock-fn.js
@@ -4,11 +4,15 @@ const utils = require('../utils')
 test('returns winner', () => {
     const originalGetWinner = utils.getWinner
 
-    utils.getWinner = (p1, p2) => p1
-
+    utils.getWinner = jest.fn((p1, p2) => p1)
+    // console.log(utils.getWinner)
     const winner = thumbWar('Cliff', 'Not Cliff')
     expect(winner).toBe('Cliff')
 
+    expect(utils.getWinner.mock.calls).toEqual([
+        ['Cliff', 'Not Cliff'],
+        ['Cliff', 'Not Cliff']
+    ])
     //cleanup
     utils.getWinner = originalGetWinner
 })
