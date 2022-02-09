8fa0836965e86ca4e9ff904609a4db408f98161a
CliffRobinson <clifford.f.robinson@gmail.com>
2022-02-09 13:01:26 +1300

    end of vid 4

--- src/no-framework/spy.js
+++ src/no-framework/spy.js
@@ -2,17 +2,29 @@ const assert = require('assert')
 const thumbWar = require('../thumb-war')
 const utils = require('../utils')
 
-function fn(impl) {
+function fn(impl = () => {}) {
     const mockFn = (...args) => {
         mockFn.mock.calls.push(args)
         return impl(...args)
     }
     mockFn.mock = {calls:[]}
+
+    mockFn.mockImplementation = newImpl => (impl = newImpl)
+    //why does doing only this change the function output after it has been returned?
+    //I guess because it re-checks impl in this scope every time it runs?
+    //weird. 
     return mockFn
 }
 
-const originalGetWinner = utils.getWinner
-utils.getWinner = fn((p1, p2) => p2)
+function spyOn(obj, prop) {
+    const originalValue = obj[prop]
+    obj[prop] = fn()
+
+    obj[prop].mockRestore = () => (obj[prop] = originalValue)
+}
+
+spyOn(utils, 'getWinner')
+utils.getWinner.mockImplementation((p1, p2) => p2)
 
 const winner = thumbWar('Injustice', 'Cliff')
 
@@ -23,4 +35,4 @@ assert.deepStrictEqual(utils.getWinner.mock.calls, [
 ])
 
 //cleanup
-utils.getWinner = originalGetWinner
+utils.getWinner.mockRestore()
