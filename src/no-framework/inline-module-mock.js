150469d7327c91c621ad798b4a0bd1561f68349e
CliffRobinson <clifford.f.robinson@gmail.com>
2022-02-21 16:23:07 +1300

    end of vid 5

--- src/no-framework/inline-module-mock.js
+++ src/no-framework/inline-module-mock.js
@@ -1,7 +1,3 @@
-const assert = require('assert')
-const thumbWar = require('../thumb-war')
-const utils = require('../utils')
-
 function fn(impl = () => {}) {
     const mockFn = (...args) => {
         mockFn.mock.calls.push(args)
@@ -15,14 +11,22 @@ function fn(impl = () => {}) {
     return mockFn
 }
 
-function spyOn(obj, prop) {
-    const originalValue = obj[prop]
-    obj[prop] = fn()
-    obj[prop].mockRestore = () => (obj[prop] = originalValue)
+const utilsPath =  require.resolve('../utils')
+
+require.cache[utilsPath] = {
+    id: utilsPath,
+    filename: utilsPath,
+    loaded: true,
+    exports: {
+        getWinner: fn((p1, p2) => p1) 
+    }
 }
 
-spyOn(utils, 'getWinner')
-utils.getWinner.mockImplementation((p1, p2) => p1)
+
+const assert = require('assert')
+const thumbWar = require('../thumb-war')
+const utils = require('../utils')
+
 const winner = thumbWar('Cliff', 'Oppression')
 assert.strictEqual(winner, 'Cliff')
 assert.deepStrictEqual(utils.getWinner.mock.calls, [
@@ -31,4 +35,4 @@ assert.deepStrictEqual(utils.getWinner.mock.calls, [
 ])
 
 //cleanup
-utils.getWinner.mockRestore()
+delete require.cache[utilsPath]
